2401166d8982bbef71ed02a9bb735b623816488e
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-04-20 10:37:23 -0500

    Banner: navbar update & banner animated text

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,14 +1,102 @@
+import banner from "@/assets/banner-img.svg";
+import "animate.css";
+import { useEffect, useState } from "react";
+import { BsArrowRightCircle } from "react-icons/bs";
+import TrackVisibility from "react-on-screen";
+
 export default function Banner() {
+	const [loopNum, setLoopNum] = useState(0);
+	const [isDeleting, setIsDeleting] = useState(false);
+	const [text, setText] = useState("");
+	const [delta, setDelta] = useState(300 - Math.random() * 100);
+	const [index, setIndex] = useState(1);
+	const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+	const period = 2000;
+
+	useEffect(() => {
+		let ticker = setInterval(() => {
+			tick();
+		}, delta);
+
+		return () => {
+			clearInterval(ticker);
+		};
+	}, [text]);
+
+	const tick = () => {
+		let i = loopNum % toRotate.length;
+		let fullText = toRotate[i];
+		let updatedText = isDeleting
+			? fullText.substring(0, text.length - 1)
+			: fullText.substring(0, text.length + 1);
+
+		setText(updatedText);
+
+		if (isDeleting) {
+			setDelta((prevDelta) => prevDelta / 2);
+		}
+
+		if (!isDeleting && updatedText === fullText) {
+			setIsDeleting(true);
+			setIndex((prevIndex) => prevIndex - 1);
+			setDelta(period);
+		} else if (isDeleting && updatedText === "") {
+			setIsDeleting(false);
+			setLoopNum(loopNum + 1);
+			setIndex(1);
+			setDelta(500);
+		} else {
+			setIndex((prevIndex) => prevIndex + 1);
+		}
+	};
+
 	return (
 		<section
 			id="home"
-			className="w-full h-screen px-24 bg-[url(@/assets/bg-banner.png)] bg-cover bg-no-repeat text-white"
+			className="w-full h-screen bg-[url(@/assets/bg-banner.png)] lg:px-24 md:px-5 lg:bg-top md:bg-center bg-cover bg-no-repeat text-white"
 		>
-			<div id="hero" className="fixed top-64 flex">
-				<div>Welcome!</div>
-				<div>
-					<img src="" alt="" />
-				</div>
+			<div id="hero" className="fixed top-64 flex flex-row">
+				<TrackVisibility className="w-[50%]">
+					{({ isVisible }) => (
+						<div
+							className={isVisible ? "animate__animated animate__fadeIn" : ""}
+						>
+							<span className="tagline">Welcome to my Portfolio</span>
+							<h1>
+								{`Hi! I'm Judy`}{" "}
+								<span
+									className="txt-rotate"
+									// dataPeriod="1000"
+									data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'
+								>
+									<span className="wrap">{text}</span>
+								</span>
+							</h1>
+							<p>
+								Lorem Ipsum is simply dummy text of the printing and typesetting
+								industry. Lorem Ipsum has been the industry's standard dummy
+								text ever since the 1500s, when an unknown printer took a galley
+								of type and scrambled it to make a type specimen book.
+							</p>
+							<button className="flex flex-row">
+								Let’s Connect <BsArrowRightCircle size={25} />
+							</button>
+						</div>
+					)}
+				</TrackVisibility>
+				<TrackVisibility className="w-[50%] bg-white">
+					{({ isVisible }) => (
+						<div
+							className={isVisible ? "animate__animated animate__zoomIn" : ""}
+						>
+							<img
+								className="w-full h-full object-contain"
+								src={banner}
+								alt="astronaut floating in space"
+							/>
+						</div>
+					)}
+				</TrackVisibility>
 			</div>
 		</section>
 	);
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,20 +13,20 @@ export default function Navbar() {
 	return (
 		<nav
 			id="navbar"
-			className="absolute top-7 w-full lg:px-24 bg-transparent text-white flex flex-row justify-between items-center"
+			className="absolute top-7 w-full lg:px-24 md:px-5 bg-transparent text-white flex flex-row justify-between items-center"
 		>
-			<div className="w-[40%]">
+			<div className="lg:w-[40%] flex">
 				<a href="">
 					<img src={logo} alt="logo" className="w-32 p-2" />
 				</a>
 			</div>
-			<div className="w-[60%] flex flex-row justify-between items-center">
-				<div className="w-[45%] flex justify-around">
+			<div className="lg:w-[60%] flex flex-row justify-between items-center">
+				<div className="lg:w-[45%] flex justify-around">
 					<NavLink href="#home">Home</NavLink>
 					<NavLink>Skills</NavLink>
 					<NavLink>Projects</NavLink>
 				</div>
-				<div className="w-[55%] flex justify-end items-center">
+				<div className="lg:w-[55%] flex justify-end items-center">
 					<div>
 						<NavBubble className="mx-1 h-9 w-9">
 							<img className="w-[40%] h-[40%]" src={navicon1} alt="" />
