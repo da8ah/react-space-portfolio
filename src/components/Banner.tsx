0e5979db6fc94f70fd85f95a918710c4a9f0dbb3
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-04-21 22:37:46 -0500

    Banner & Skills sections

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,9 +3,9 @@ import Button from "@/components/ui/Button";
 import "animate.css";
 import { useEffect, useState } from "react";
 import { BsArrowRightCircle } from "react-icons/bs";
-import TrackVisibility from "react-on-screen";
 
 export default function Banner() {
+	const [isVisible, setVisibility] = useState(false);
 	const [loopNum, setLoopNum] = useState(0);
 	const [isDeleting, setIsDeleting] = useState(false);
 	const [text, setText] = useState("");
@@ -13,6 +13,10 @@ export default function Banner() {
 	const [index, setIndex] = useState(1);
 	const toRotate = ["Full-Stack Developer", "Web Designer", "UI/UX Designer"];
 	const period = 2000;
+
+	useEffect(() => {
+		setVisibility(true);
+	}, []);
 
 	useEffect(() => {
 		let ticker = setInterval(() => {
@@ -55,48 +59,44 @@ export default function Banner() {
 			className="w-full h-screen bg-fixed bg-[url(@/assets/bg-banner.png)] lg:px-24 lg:bg-top md:px-5 md:bg-center bg-cover bg-no-repeat text-white"
 		>
 			<div id="hero" className="relative top-64 flex flex-row items-start">
-				<TrackVisibility className="w-[55%]">
-					{({ isVisible }) => (
-						<div
-							className={isVisible ? "animate__animated animate__fadeIn" : ""}
-						>
-							<span className="text-2xl font-bold font tracking-wide p-2 ring-1 ring-white bg-gradient-to-r from-[#AA367C] to-[#4A2FBD]">
-								Welcome to my Portfolio
+				<div className="w-[55%]">
+					<div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+						<span className="shadow-lg shadow-purple-500 text-2xl font-bold font tracking-wide p-2 ring-1 ring-white bg-gradient-to-r from-[#AA367C] to-[#4A2FBD]">
+							Welcome to my Portfolio
+						</span>
+						<h1 className="my-5 py-1 text-7xl font-bold drop-shadow-[0_1px_0_rgba(0,0,0,0.2)]">
+							Hi! I'm{" "}
+							<span className="animate-[pulse_2s_linear_infinite] text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">
+								Tiber{" "}
 							</span>
-							<h1 className="my-5 text-7xl font-bold">
-								Hi! I'm{" "}
-								<span className="text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">
-									Tiber{" "}
-								</span>
-								<span className="wrap">{text}</span>
-							</h1>
-							<p className="text-gray-400 tracking-widest">
-								Lorem Ipsum is simply dummy text of the printing and typesetting
-								industry. Lorem Ipsum has been the industry's standard dummy
-								text ever since the 1500s, when an unknown printer took a galley
-								of type and scrambled it to make a type specimen book.
-							</p>
-							<Button className="flex flex-row mt-10 text-white font-bold">
-								Let’s Connect <BsArrowRightCircle size={25} className="ml-2" />
-							</Button>
-						</div>
-					)}
-				</TrackVisibility>
-				<TrackVisibility className="w-[45%]">
-					{({ isVisible }) => (
-						<div
-							className={`flex justify-center animate-[bounce_5s_linear_infinite] ${
-								isVisible ? "animate__animated animate__zoomIn" : ""
-							}`}
-						>
-							<img
-								className="w-[90%] ml-10"
-								src={banner}
-								alt="astronaut floating in space"
-							/>
-						</div>
-					)}
-				</TrackVisibility>
+							<span className="drop-shadow-[1px_5px_0_rgb(145,47,179)]">
+								{text}
+							</span>
+						</h1>
+						<p className="text-gray-400 tracking-widest">
+							Lorem Ipsum is simply dummy text of the printing and typesetting
+							industry. Lorem Ipsum has been the industry's standard dummy text
+							ever since the 1500s, when an unknown printer took a galley of
+							type and scrambled it to make a type specimen book.
+						</p>
+						<Button className="flex flex-row mt-10 text-white font-bold">
+							Let’s Connect <BsArrowRightCircle size={25} className="ml-2" />
+						</Button>
+					</div>
+				</div>
+				<div className="w-[45%] animate-[bounce_5s_linear_infinite]">
+					<div
+						className={`flex justify-center ${
+							isVisible ? "animate__animated animate__zoomIn" : ""
+						}`}
+					>
+						<img
+							className="w-[90%] ml-10"
+							src={banner}
+							alt="astronaut floating in space"
+						/>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,10 +1,78 @@
+import meter1 from "@/assets/skills-meter1.svg";
+import meter2 from "@/assets/skills-meter2.svg";
+import meter3 from "@/assets/skills-meter3.svg";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import TrackVisibility from "react-on-screen";
+
 export default function Skills() {
+	const responsive = {
+		superLargeDesktop: {
+			// the naming can be any, depends on you.
+			breakpoint: { max: 4000, min: 3000 },
+			items: 5,
+		},
+		desktop: {
+			breakpoint: { max: 3000, min: 1024 },
+			items: 3,
+		},
+		tablet: {
+			breakpoint: { max: 1024, min: 464 },
+			items: 2,
+		},
+		mobile: {
+			breakpoint: { max: 464, min: 0 },
+			items: 1,
+		},
+	};
+
 	return (
-		<section
-			id="skills"
-			className="w-full h-screen bg-fixed bg-black bg-[url(@/assets/bg-skills.png)] lg:px-24 lg:bg-top md:px-5 md:bg-center bg-cover bg-no-repeat text-white"
-		>
-			<div className="relative top-64 flex flex-row items-start">Hola</div>
-		</section>
+		<TrackVisibility>
+			{({ isVisible }) => (
+				<section
+					id="skills"
+					className={`w-full h-screen flex pt-20 items-center transition-all bg-fixed bg-black lg:px-24 lg:bg-top md:px-5 md:bg-center bg-cover bg-no-repeat text-white
+					${
+						isVisible
+							? "bg-[url(@/assets/bg-skills.png)]"
+							: "bg-[url(@/assets/bg-banner.png)]"
+					}`}
+				>
+					<div className="rounded-[90px] top-64 w-full px-40 py-10 flex flex-col justify-center items-center bg-[#0f0f0f]">
+						<h2 className="text-7xl font-bold">Skills</h2>
+						<p className="my-5 text-gray-400 tracking-widest text-center">
+							Lorem Ipsum is simply dummy text of the printing and typesetting
+							industry.<br></br> Lorem Ipsum has been the industry's standard
+							dummy text.
+						</p>
+						<Carousel
+							responsive={responsive}
+							infinite
+							autoPlay
+							autoPlaySpeed={3000}
+							transitionDuration={500}
+							className="w-full mt-16 owl-carousel owl-theme text-center"
+						>
+							<div className="flex flex-col justify-center items-center">
+								<img className="w-2/4 mb-5" src={meter1} alt="Image" />
+								<h5>Backend Development</h5>
+							</div>
+							<div className="flex flex-col justify-center items-center">
+								<img className="w-2/4 mb-5" src={meter2} alt="Image" />
+								<h5>Brand Identity</h5>
+							</div>
+							<div className="flex flex-col justify-center items-center">
+								<img className="w-2/4 mb-5" src={meter3} alt="Image" />
+								<h5>Logo Design</h5>
+							</div>
+							<div className="flex flex-col justify-center items-center">
+								<img className="w-2/4 mb-5" src={meter1} alt="Image" />
+								<h5>Frontend Development</h5>
+							</div>
+						</Carousel>
+					</div>
+				</section>
+			)}
+		</TrackVisibility>
 	);
 }
