81b7102d774b5fac3c053a53e35802c41516f851
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-04-25 16:56:01 -0500

    Navbar: mobile responsive

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -56,13 +56,13 @@ export default function Banner() {
 	return (
 		<section
 			id="banner"
-			className="w-full lg:h-[110vh] md:h-[100vh] bg-fixed bg-[url(@/assets/bg-banner.png)] lg:px-24 lg:bg-top md:px-5 md:bg-center bg-cover bg-no-repeat text-white"
+			className="w-full lg:h-[110vh] md:h-[100vh] bg-fixed bg-[url(@/assets/bg-banner.png)] lg:px-24 lg:bg-top md:px-5 px-1 md:bg-center bg-cover bg-no-repeat text-white"
 		>
 			<div
 				id="hero"
-				className="relative lg:top-64 md:top-52 flex flex-row items-start"
+				className="relative lg:top-64 md:top-52 top-20 flex md:flex-row flex-col-reverse items-start"
 			>
-				<div className="w-[55%]">
+				<div className="md:w-[55%]">
 					<div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
 						<span className="shadow-lg shadow-purple-500 lg:text-2xl md:text-xl font-bold font tracking-wide p-2 ring-1 ring-white bg-gradient-to-r from-[#AA367C] to-[#4A2FBD]">
 							Welcome to my Portfolio
@@ -89,7 +89,7 @@ export default function Banner() {
 						</Button>
 					</div>
 				</div>
-				<div className="w-[45%] animate-[updown_5s_linear_infinite]">
+				<div className="md:w-[45%] animate-[updown_5s_linear_infinite]">
 					<div
 						className={`flex justify-center lg:items-start md:items-center ${
 							isVisible ? "animate__animated animate__zoomIn" : ""
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,8 +6,10 @@ import NavBubble from "@/components/ui/NavBubble";
 import NavButton from "@/components/ui/NavButton";
 import NavLink from "@/components/ui/NavLink";
 import { useEffect, useState } from "react";
+import { RxLayers, RxStack } from "react-icons/rx";
 
 export default function Navbar() {
+	const [isMenuOpen, setMenuState] = useState<boolean>(false);
 	const [isFixed, setPositionToFixed] = useState<boolean>(false);
 
 	useEffect(() => {
@@ -29,11 +31,13 @@ export default function Navbar() {
 			id="navbar"
 			className={`${
 				isFixed
-					? "fixed pt-5 opacity-50 hover:opacity-100 hover:transition-opacity hover:bg-[#121212]"
-					: "absolute pt-7 bg-transparent"
-			} z-[100] w-full pb-3 lg:px-24 md:px-5 text-white flex flex-row justify-between items-center`}
+					? "fixed w-full md:pt-5 md:opacity-50 md:hover:opacity-100 md:hover:transition-opacity hover:bg-[#121212]"
+					: "fixed md:absolute p-0 md:pt-7 md:bg-transparent"
+			} ${
+				isMenuOpen ? "h-screen opacity-100 bg-[#121212]" : "h-auto"
+			} z-[100] w-full lg:px-24 md:px-5 md:pb-3 text-white flex md:flex-row flex-col md:justify-between md:items-center`}
 		>
-			<div className="lg:w-[40%] flex">
+			<div className="lg:w-[40%] md:px-0 pr-2 flex flex-row justify-between items-center">
 				<a
 					href="https://www.youtube.com/watch?v=hYv6BM2fWd8"
 					target="_blank"
@@ -45,15 +49,40 @@ export default function Navbar() {
 						className="w-32 p-2"
 					/>
 				</a>
+				{isMenuOpen ? (
+					<RxLayers
+						focusable={false}
+						className="cursor-pointer md:hidden"
+						size={35}
+						onClick={() => setMenuState(false)}
+					/>
+				) : (
+					<RxStack
+						focusable={false}
+						className="cursor-pointer md:hidden"
+						size={35}
+						onClick={() => setMenuState(true)}
+					/>
+				)}
 			</div>
-			<div className="lg:w-[60%] flex flex-row justify-between items-center">
-				<div className="lg:w-[45%] flex justify-around">
-					<NavLink href="#banner">Home</NavLink>
-					<NavLink href="#skills">Skills</NavLink>
-					<NavLink href="#projects">Projects</NavLink>
+			<div
+				className={`${
+					isMenuOpen ? "block" : "hidden"
+				} lg:w-[60%] pt-20 md:pt-0 md:flex md:flex-row flex-col justify-between items-center`}
+			>
+				<div className="lg:w-[45%] flex md:flex-row flex-col justify-around">
+					<NavLink href="#banner" onClick={() => setMenuState(false)}>
+						Home
+					</NavLink>
+					<NavLink href="#skills" onClick={() => setMenuState(false)}>
+						Skills
+					</NavLink>
+					<NavLink href="#projects" onClick={() => setMenuState(false)}>
+						Projects
+					</NavLink>
 				</div>
-				<div className="lg:w-[55%] flex justify-end items-center">
-					<div>
+				<div className="lg:w-[55%] w-full flex md:flex-row flex-col-reverse md:justify-end items-center">
+					<div className="py-2 md:py-0">
 						<NavBubble className="mx-1 h-9 w-9">
 							<img
 								className="w-[40%] h-[40%]"
@@ -76,7 +105,11 @@ export default function Navbar() {
 							/>
 						</NavBubble>
 					</div>
-					<NavButton href="#contact" className="mr-1 ml-12 px-9 py-4">
+					<NavButton
+						href="#contact"
+						className="md:my-0 md:mr-1 md:ml-12 md:px-9 md:py-4 my-5 p-2"
+						onClick={() => setMenuState(false)}
+					>
 						Let's Connect
 					</NavButton>
 				</div>
